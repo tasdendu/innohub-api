9fc9ac531dcfc7371c0efc6d464354cbdf204cb7
Tashi Dendup <tashi.dendup@dcpl.bt>
2022-10-13 15:51:48 +0600

    Seed cateory

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,6 +28,30 @@ desc 'Seeding users'
   user.confirm
 end
 
+puts 'Seeding category'
+[
+  { name: 'Environmental', id: 1 },
+  { name: 'Health', id: 2 },
+  { name: 'Education', id: 3 },
+  { name: 'Agriculture', id: 4 },
+  { name: 'Sports', id: 5 },
+  { name: 'Religion', id: 6 },
+  { name: 'Lifestyle', id: 7 },
+  { name: 'Media', id: 8 },
+  { name: 'Event', id: 9 },
+  { name: 'Politics', id: 10 },
+  { name: 'Business', id: 11 },
+  { name: 'Startups', id: 12 },
+  { name: 'Entertainment', id: 13 },
+  { name: 'Court/Legal System', id: 14 },
+  { name: 'Immigration', id: 15 },
+  { name: 'Race/Gender', id: 16 },
+  { name: 'Financial', id: 17 },
+  { name: 'Technology', id: 18 }
+].each do |c|
+  Category.find_or_create_by(c)
+end
+
 ActiveRecord::Base.connection.tables.each do |t|
   ActiveRecord::Base.connection.reset_pk_sequence!(t)
 end
